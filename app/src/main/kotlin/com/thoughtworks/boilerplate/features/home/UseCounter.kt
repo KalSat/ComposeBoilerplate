1cae954bd71c4a1ba29b9884c740771595f3aa47
Steve Cheng <stevecheng@yeah.net>
2026-06-11 16:24:26 +0800

    refactor: add hooks libs

--- app/src/main/kotlin/com/thoughtworks/boilerplate/features/home/UseCounter.kt
+++ app/src/main/kotlin/com/thoughtworks/boilerplate/features/home/UseCounter.kt
@@ -2,19 +2,18 @@ package com.thoughtworks.boilerplate.features.home
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.thoughtworks.boilerplate.utils.Tuple2
 import com.thoughtworks.boilerplate.utils.tupleOf
 import kotlin.reflect.KFunction0
+import xyz.junerver.compose.hooks.useState
 
 /**
  * @author steve  2026/6/11
  */
 @Composable
 fun useCounter(): Tuple2<Int, KFunction0<Unit>> {
-    var count by remember { mutableIntStateOf(0) }
+    var count by useState(0)
 
     fun increment() {
         count++
